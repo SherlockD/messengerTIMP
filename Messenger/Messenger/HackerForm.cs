8a1514fe29acd641d9555d872775ebd597cd7c95
SherlockD <ryavi1@mail.com>
2019-02-27 14:07:04 +0300

    Added Hacker part

---
+++ Messenger/Messenger/HackerForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Messenger
+{
+    public partial class HackerForm : Form
+    {
+        private string port;
+
+        public HackerForm()
+        {
+            FormClosed += new FormClosedEventHandler(OnFormClosed);
+            InitializeComponent();
+            var _reciveThread = new Thread(new ThreadStart(ReceiveMessage));
+            _reciveThread.Start();
+        }
+
+        public void OnFormClosed(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void ReceiveMessage()
+        {
+            UdpClient udpClient = new UdpClient(8000);
+            IPEndPoint remoteIp = null;
+            try
+            {
+                while (true)
+                {
+
+                    byte[] data = udpClient.Receive(ref remoteIp);
+                    string message = Encoding.Unicode.GetString(data);
+
+                    if (message.Substring(5) == "Exit")
+                    {
+                        if (message.Substring(0, 4) == port)
+                        {
+                            Invoke((MethodInvoker)(() => label3.Text = "Status: offline"));
+                        }
+                        LastExitSerialization.Serialize(message.Substring(0, 4),DateTime.Now.ToLongDateString() + DateTime.Now.ToLongTimeString());
+                    }
+
+                    if (message.Substring(5) == "Open")
+                    {
+                        if (message.Substring(0, 4) == port)
+                        {
+                            Invoke((MethodInvoker)(() => label3.Text = "Status: online"));
+                            //return;
+                        }
+                    }
+
+                    if (!string.IsNullOrEmpty(message) && message.Substring(0, 4) == port)
+                    {
+                        Invoke((MethodInvoker)(() => label3.Text = "Status: online"));
+
+                        Invoke((MethodInvoker)(() => listBox2.Items.Add($"{message}")));
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Status: offline";
+            port = textBox1.Text;
+            var lastData = LastExitSerialization.GetPortData(port);
+
+            label2.Text = $"Дата последнего входа: {(lastData != string.Empty ? lastData : "Невозможно определить")}";
+        }
+
+        private void HackerForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Messenger/Messenger/LastExitSerialization.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace Messenger
+{
+    static class LastExitSerialization
+    {
+        public static void Serialize(string port,string data)
+        {
+            if (!File.Exists("serialize.txt"))
+            {
+                var file = File.Create("serialize.txt");
+                file.Close();
+            }
+
+            var readedLines = File.ReadAllLines("serialize.txt").ToList();
+            for(int i = 0; i < readedLines.Count; i++)
+            {
+                if(readedLines[i].Substring(0,4) == port)
+                {
+                    readedLines[i] =  $"{port}:{data}";
+                    File.WriteAllLines("serialize.txt", readedLines.ToArray());
+                    return;
+                }
+            }
+            using(var streamWriter = new StreamWriter("serialize.txt",true))
+            {
+                streamWriter.WriteLine($"{port}:{data}");
+            }
+        }
+
+        public static string GetPortData(string port)
+        {
+            var streamReader = new StreamReader("serialize.txt");
+            while (!streamReader.EndOfStream)
+            {
+                var line = streamReader.ReadLine();
+                if(line.Substring(0,4) == port)
+                {
+                    streamReader.Close();
+                    return line.Substring(5);
+                }
+            }
+            streamReader.Close();
+            return string.Empty;
+        }
+    }
+}
